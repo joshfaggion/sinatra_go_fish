f3e10482bcf8c831d87a9c35ef01dd78564061ae
Joshua Faggion <joshua.faggion@gmail.com>
2018-06-18 19:38:48 -0400

    Write tests for app.rb, create css, create layout.

--- app.rb
+++ app.rb
@@ -1,6 +1,16 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'pry'
 
 get('/') do
+  redirect('/join')
+end
+
+get('/join') do
+  slim(:join_game)
+end
+
+post('/game') do
+  @name = params['name']
   slim(:index)
 end
---
+++ integration_spec.rb
@@ -0,0 +1,21 @@
+require 'sinatra'
+require 'pry'
+require 'capybara/rspec'
+require_relative 'app.rb'
+Capybara.app = Sinatra::Application
+
+
+describe "#Join Page", {:type => :feature} do
+  it 'should be able to go to the join page' do
+    visit('/join')
+    expect(page).to have_content('-Join Game-')
+  end
+
+  it 'should be able to send it to a waiting page' do
+    visit('/join')
+    fill_in :name, with: 'Josh'
+    click_on "submit"
+    expect(page).to have_content "Josh"
+    expect(page).to have_content "Players Waiting..."
+  end
+end
